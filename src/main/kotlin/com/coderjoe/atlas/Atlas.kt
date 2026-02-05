de2cd5c99963b9f76f5f5b280eef9de9cd518140
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-05 01:17:26 -0600

    Initialize power system and implement auto-save functionality for power blocks

--- src/main/kotlin/com/coderjoe/atlas/Atlas.kt
+++ src/main/kotlin/com/coderjoe/atlas/Atlas.kt
@@ -1,10 +1,18 @@
 package com.coderjoe.atlas
 
+import com.coderjoe.atlas.power.PowerBlockInitializer
+import com.coderjoe.atlas.power.PowerBlockListener
+import com.coderjoe.atlas.power.PowerBlockPersistence
+import com.coderjoe.atlas.power.PowerBlockRegistry
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitTask
 
 class Atlas : JavaPlugin() {
     private lateinit var resourcePackManager: ResourcePackManager
     private lateinit var nexoIntegration: NexoIntegration
+    private lateinit var powerBlockRegistry: PowerBlockRegistry
+    private lateinit var powerBlockPersistence: PowerBlockPersistence
+    private var autoSaveTask: BukkitTask? = null
 
     override fun onEnable() {
         if (!dataFolder.exists()) {
@@ -22,10 +30,41 @@ class Atlas : JavaPlugin() {
             server.pluginManager.registerEvents(PlayerJoinListener(resourcePackManager), this)
         }
 
+        initPowerSystem()
+
         logger.info("Atlas plugin enabled!")
     }
 
     override fun onDisable() {
+        // Cancel auto-save task
+        autoSaveTask?.cancel()
+
+        // Save all power blocks before shutdown
+        if (::powerBlockPersistence.isInitialized && ::powerBlockRegistry.isInitialized) {
+            powerBlockPersistence.save(powerBlockRegistry)
+        }
+
+        // Stop all power block ticking
+        if (::powerBlockRegistry.isInitialized) {
+            powerBlockRegistry.stopAll()
+        }
+
         logger.info("Atlas plugin has been disabled!")
+    }
+
+    fun initPowerSystem() {
+        PowerBlockInitializer.initialize(this)
+        powerBlockRegistry = PowerBlockRegistry(this)
+        powerBlockPersistence = PowerBlockPersistence(this)
+        powerBlockPersistence.load(powerBlockRegistry)
+
+        server.pluginManager.registerEvents(PowerBlockListener(this, powerBlockRegistry), this)
+
+        // Auto-save every 5 minutes (6000 ticks)
+        autoSaveTask = server.scheduler.runTaskTimer(this, Runnable {
+            powerBlockPersistence.save(powerBlockRegistry)
+        }, 6000L, 6000L)
+
+        logger.info("Power system initialized")
     }
 }
