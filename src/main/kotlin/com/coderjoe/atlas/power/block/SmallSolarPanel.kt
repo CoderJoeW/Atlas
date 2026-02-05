52f0732c6bfefd0b008b28d51d14bcabfe6eb00d
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-05 01:23:37 -0600

    Add SmallSolarPanel class to handle power generation during daytime

---
+++ src/main/kotlin/com/coderjoe/atlas/power/block/SmallSolarPanel.kt
@@ -0,0 +1,28 @@
+package com.coderjoe.atlas.power.block
+
+import com.coderjoe.atlas.power.PowerBlock
+import com.coderjoe.atlas.power.PowerBlockFactory
+import org.bukkit.Location
+
+class SmallSolarPanel(location: Location): PowerBlock(location, maxStorage = 10) {
+
+    companion object {
+        const val BLOCK_ID = "small_solar_panel"
+    }
+
+    override fun powerUpdate() {
+        val world = location.world ?: return
+        val isDaytime = world.time in 0..12000
+
+        if (isDaytime) {
+            val generated = addPower(1)
+            if (generated > 0) {
+                plugin.logger.info("SmallSolarPanel at ${location.blockX},${location.blockY},${location.blockZ} generated $generated power (now $currentPower/$maxStorage)")
+            }
+        } else {
+            plugin.logger.info("SmallSolarPanel at ${location.blockX},${location.blockY},${location.blockZ} is not generating power because it is not daytime.")
+        }
+
+        // TODO: Implement power transfer to connected blocks
+    }
+}
