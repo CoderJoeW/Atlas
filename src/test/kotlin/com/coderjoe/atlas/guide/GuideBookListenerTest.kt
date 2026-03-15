fe6acca176eabfb4200e5ed1c24c4f9fc5e1feb1
CoderJoe <jwilliamson@jslcodelabs.com>
2026-03-14 22:58:27 -0500

    Refactor mock imports in GuideBookListenerTest for clarity and specificity

--- src/test/kotlin/com/coderjoe/atlas/guide/GuideBookListenerTest.kt
+++ src/test/kotlin/com/coderjoe/atlas/guide/GuideBookListenerTest.kt
@@ -1,12 +1,20 @@
 package com.coderjoe.atlas.guide
 
 import com.coderjoe.atlas.TestHelper
-import io.mockk.*
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import io.mockk.verify
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import java.util.UUID
 
 class GuideBookListenerTest {
