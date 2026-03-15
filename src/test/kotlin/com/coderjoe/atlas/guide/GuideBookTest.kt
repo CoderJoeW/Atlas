64d3e727db3131d2955e42eafc52420df348f36d
CoderJoe <jwilliamson@jslcodelabs.com>
2026-03-14 22:57:12 -0500

    Explicitly set jupiter api imports instead of using wildcard

--- src/test/kotlin/com/coderjoe/atlas/guide/GuideBookTest.kt
+++ src/test/kotlin/com/coderjoe/atlas/guide/GuideBookTest.kt
@@ -14,7 +14,7 @@ import org.bukkit.World
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class GuideBookTest {
 
