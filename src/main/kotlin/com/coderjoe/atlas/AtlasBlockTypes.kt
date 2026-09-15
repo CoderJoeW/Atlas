ee89c382b197853021f053d328004661857f0988
CoderJoe <jwilliamson@jslcodelabs.com>
2026-09-14 20:43:15 -0500

    Introduce BlockCatalog and a single AtlasBlockTypes list

---
+++ src/main/kotlin/com/coderjoe/atlas/AtlasBlockTypes.kt
@@ -0,0 +1,45 @@
+package com.coderjoe.atlas
+
+import com.coderjoe.atlas.block.BlockCatalog
+import com.coderjoe.atlas.block.fluid.block.FluidContainer
+import com.coderjoe.atlas.block.fluid.block.FluidPipe
+import com.coderjoe.atlas.block.fluid.block.FluidPump
+import com.coderjoe.atlas.block.power.LavaGenerator
+import com.coderjoe.atlas.block.power.PowerCable
+import com.coderjoe.atlas.block.power.SmallBattery
+import com.coderjoe.atlas.block.power.SmallSolarPanel
+import com.coderjoe.atlas.block.power.factory.CobblestoneFactory
+import com.coderjoe.atlas.block.power.factory.ObsidianFactory
+import com.coderjoe.atlas.block.power.mine.CoalMine
+import com.coderjoe.atlas.block.power.mine.DiamondMine
+import com.coderjoe.atlas.block.power.mine.EmeraldMine
+import com.coderjoe.atlas.block.power.mine.GoldMine
+import com.coderjoe.atlas.block.power.mine.IronMine
+import com.coderjoe.atlas.block.power.mine.NetheriteMine
+import com.coderjoe.atlas.block.power.mine.RedstoneMine
+import com.coderjoe.atlas.block.transport.block.ConveyorBelt
+
+object AtlasBlockTypes {
+    val catalog =
+        BlockCatalog(
+            listOf(
+                SmallSolarPanel.descriptor,
+                SmallBattery.descriptor,
+                PowerCable.descriptor,
+                LavaGenerator.descriptor,
+                CobblestoneFactory.descriptor,
+                ObsidianFactory.descriptor,
+                CoalMine.descriptor,
+                IronMine.descriptor,
+                RedstoneMine.descriptor,
+                GoldMine.descriptor,
+                EmeraldMine.descriptor,
+                DiamondMine.descriptor,
+                NetheriteMine.descriptor,
+                FluidPump.descriptor,
+                FluidPipe.descriptor,
+                FluidContainer.descriptor,
+                ConveyorBelt.descriptor,
+                )
+        )
+}
